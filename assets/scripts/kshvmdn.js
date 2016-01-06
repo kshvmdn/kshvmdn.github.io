a654eb2331767c458ae377d74abc3e1949f17a71
Kashav Madan <kshvmdn@gmail.com>
2016-01-06 16:48:11 -0500

    Wording, sub-hero styling + name type effect on load

--- assets/scripts/kshvmdn.js
+++ assets/scripts/kshvmdn.js
@@ -1,5 +1,22 @@
 $(function() {
-	setInterval(randomWord, 1500);
+
+	var $words = $('.hero .words');
+	var $underscore = $('.hero .underscore');
+	var $name = $('.hero .name');
+
+	var words = ['developer', 'hacker', 'designer', 'innovater', 'creator', 'builder', 'technology enthusiast', 'leader']
+	var underscore = '_';
+	var name = 'Kashav.'.split('');
+
+	typeName($name, 'Kashav.'.split(''), 0);
+
+	setTimeout(function() {
+		setInterval(function() {
+			$words.html(words[Math.floor(Math.random() * words.length)]);
+			// flash($underscore, underscore);
+		}, 650);
+	}, name.length * 301);
+
 	$('.contact_trigger').click(function(){
 		$('.contact').addClass('blink');
 		setTimeout(function(){
@@ -8,7 +25,15 @@ $(function() {
 	});
 });
 
-function randomWord() {
-	words = ['develop', 'hack', 'design', 'write code', 'innovate', 'create', 'build']
-	$('.words').html(words[Math.floor(Math.random() * words.length)]);
+var flash = function(el, text) {
+	el.text() != text ? el.html(text) : el.html('');
+}
+
+var typeName = function(el, array, arrayPosition) {
+	if (arrayPosition < array.length) {
+		setTimeout(function () {
+			el.append(array[arrayPosition]);
+			typeName(el, array, arrayPosition + 1);
+		}, 300);
+	}
 }
