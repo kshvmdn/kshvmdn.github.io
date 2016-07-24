8279657342f872830a349977457313fbbf1c3212
Kashav Madan <kshvmdn@gmail.com>
2016-07-24 14:03:22 -0400

    Clean up JS
    No more semi-colons 😝

--- assets/scripts/kshvmdn.js
+++ assets/scripts/kshvmdn.js
@@ -4,89 +4,92 @@ var TYPE_CLASSES = {
   str: 'json string',
   bool: 'json boolean',
   none: 'json null'
-};
+}
 
 function getJson (url, cb) {
-  var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest()
 
-  xhr.open('get', url, true);
+  xhr.open('get', url, true)
   xhr.onreadystatechange = function() {
-    var status;
+    var status = void(0)
 
     if (xhr.readyState === 4) {
-      status = xhr.status;
+      status = xhr.status
       if (status != 200) {
-        return cb(status);
+        return cb(status)
       }
 
-      var data = JSON.parse(xhr.responseText);
-      cb(null, data);
+      var data = JSON.parse(xhr.responseText)
+      cb(null, data)
     }
-  };
-  xhr.send();
+  }
+  xhr.send()
 }
 
 function replaceJson (match, indent, key, val, end) {
-  var start = indent || '';
-  var end = end || '';
+  var start = indent || ''
+  var end = end || ''
 
   if (key) {
-    $key = document.createElement('span');
-    $key.setAttribute('class', TYPE_CLASSES.key);
-    $key.innerHTML = key.replace(/[": ]/g, '') + ': ';
+    $key = document.createElement('span')
+    $key.setAttribute('class', TYPE_CLASSES.key)
+    $key.innerHTML = key.replace(/[": ]/g, '') + ': '
 
-    start += $key.outerHTML;
+    start += $key.outerHTML
   }
 
   if (val) {
-    var type;
+    var type = void(0)
 
     switch(typeof eval(val)) {
-      case 'string': type = 'str'; break;
-      case 'boolean': type = 'bool'; break;
-      case 'number': type = 'val'; break;
-      default: type = 'none'; break;
+      case 'string':
+        type = 'str'
+        break
+      case 'boolean':
+        type = 'bool'
+        break
+      case 'number':
+        type = 'val'
+        break
+      default:
+        type = 'none'
+        break
     }
 
     if (/(https?:\/\/[^\s]+)|.\/assets/g.test(val)) {
-      val = val.replace(/"/g, '');
+      val = val.replace(/"/g, '')
 
-      var $a = document.createElement('a');
-      $a.setAttribute('href', val);
-      $a.innerHTML = /.\/assets/g.test(val) ? val.substr(val.lastIndexOf('/') + 1) : val;
+      var $a = document.createElement('a')
+      $a.setAttribute('href', val)
+      $a.innerHTML = /.\/assets/g.test(val) ? val.substr(val.lastIndexOf('/') + 1) : val
 
-      val = '"' + $a.outerHTML + '"';
+      val = '"' + $a.outerHTML + '"'
     }
 
-    var $val = document.createElement('span');
-    $val.setAttribute('class', TYPE_CLASSES[type]);
-    $val.innerHTML = val;
+    var $val = document.createElement('span')
+    $val.setAttribute('class', TYPE_CLASSES[type])
+    $val.innerHTML = val
 
-    start += $val.outerHTML;
+    start += $val.outerHTML
   }
 
-  return start + end;
+  return start + end
 }
 
 function prettyPrint (obj) {
-  var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-  return JSON.stringify(obj, null, 2)
-    .replace(/&/g, '&amp;')
-    .replace(/\\"/g, '&quot;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(jsonLine, replaceJson);
+  var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg
+  return JSON.stringify(obj, null, 2).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(jsonLine, replaceJson)
 }
 
 window.onload = function () {
   getJson('./assets/docs/kshvmdn.json', function(err, res) {
-    if (err || !res) res = {};
+    if (err || !res) res = {}
 
     for (var k in res) {
-      var $el = document.getElementById(k + '-json');
-      $el.innerHTML = prettyPrint(res[k]);
+      var $el = document.getElementById(k + '-json')
+      $el.innerHTML = prettyPrint(res[k])
     }
 
-    document.querySelector('.console').style.display = 'block';
-  });
-};
+    document.querySelector('.console').style.display = 'block'
+  })
+}
