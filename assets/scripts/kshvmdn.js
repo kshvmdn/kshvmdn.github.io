9493bb683ecea715a8f15a8e20602365cd521954
Kashav Madan <kshvmdn@gmail.com>
2016-07-31 01:50:09 -0400

    Update content, use new resume

--- assets/scripts/kshvmdn.js
+++ assets/scripts/kshvmdn.js
@@ -1,92 +1,98 @@
-(function() {
-  var TYPE_CLASSES, getJson, prettyPrint, replaceJson;
+var TYPES = {
+  key: 'json key',
+  val: 'json value',
+  str: 'json string',
+  bool: 'json boolean',
+  none: 'json null'
+};
 
-  TYPE_CLASSES = {
-    key: 'json key',
-    val: 'json value',
-    str: 'json string',
-    bool: 'json boolean',
-    none: 'json null'
-  };
+function getJson (url, cb) {
+  var xhr;
+  xhr = new XMLHttpRequest;
+  xhr.open('get', url, true);
+  xhr.onreadystatechange = function () {
+    var data, status;
 
-  getJson = function(url, cb) {
-    var xhr;
-    xhr = new XMLHttpRequest;
-    xhr.open('get', url, true);
-    xhr.onreadystatechange = function() {
-      var data, status;
-      status = void 0;
-      if (xhr.readyState === 4) {
-        status = xhr.status;
-        if (status !== 200) {
-          return cb(status);
-        }
-        data = JSON.parse(xhr.responseText);
-        cb(null, data);
-      }
-    };
-    xhr.send();
-  };
+    if (xhr.readyState === 4) {
+      status = xhr.status;
 
-  replaceJson = function(match, indent, key, val, end) {
-    var $a, $key, $val, body, head, tail, type;
-    body = '';
-    head = indent || '';
-    tail = end || '';
-    if (key) {
-      $key = document.createElement('span');
-      $key.setAttribute('class', TYPE_CLASSES.key);
-      $key.innerHTML = key.replace(/[": ]/g, '') + ': ';
-      body += $key.outerHTML;
+      if (status !== 200) return cb(status);
+
+      data = JSON.parse(xhr.responseText);
+      return cb(null, data);
     }
-    if (val) {
-      switch (typeof eval(val)) {
-        case 'string':
-          type = 'str';
-          break;
-        case 'boolean':
-          type = 'bool';
-          break;
-        case 'number':
-          type = 'val';
-          break;
-        default:
-          type = 'none';
-          break;
-      }
-      if (/(https?:\/\/[^\s]+)|.\/assets/g.test(val)) {
-        val = val.replace(/"/g, '');
-        $a = document.createElement('a');
-        $a.setAttribute('href', val);
-        $a.innerHTML = /.\/assets/g.test(val) ? val.substr(val.lastIndexOf('/') + 1) : val;
-        val = '"' + $a.outerHTML + '"';
-      }
-      $val = document.createElement('span');
-      $val.setAttribute('class', TYPE_CLASSES[type]);
-      $val.innerHTML = val;
-      body += $val.outerHTML;
+  };
+  xhr.send();
+}
+
+function replacer (match, indent, key, val, end) {
+  var $a, $key, $val, body, head, tail, type;
+
+  body = '';
+  head = indent || '';
+  tail = end || '';
+
+  if (key) {
+    $key = document.createElement('span');
+    $key.setAttribute('class', TYPES.key);
+    $key.innerHTML = key.replace(/[": ]/g, '') + ': ';
+    body += $key.outerHTML;
+  }
+
+  if (val) {
+    switch (typeof eval(val)) {
+      case 'string':
+        type = 'str';
+        break;
+      case 'boolean':
+        type = 'bool';
+        break;
+      case 'number':
+        type = 'val';
+        break;
+      default:
+        type = 'none';
+        break;
     }
-    return head + body + tail;
-  };
 
-  prettyPrint = function(obj) {
-    var jsonLine;
-    jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-    return JSON.stringify(obj, null, 2).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(jsonLine, replaceJson);
-  };
+    if (/(https?:\/\/[^\s]+)|.\/assets/g.test(val)) {
+      val = val.replace(/"/g, '');
 
-  window.onload = function() {
-    getJson('./assets/docs/kshvmdn.json', function(err, res) {
-      var $el, k;
-      if (err || !res) {
-        document.querySelector('.preload').style.display = 'block';
-        return;
-      }
-      for (k in res) {
-        $el = document.getElementById(k + '-json');
-        $el.innerHTML = prettyPrint(res[k]);
-      }
-      document.querySelector('.console').style.display = 'block';
-    });
-  };
-}).call(this);
+      $a = document.createElement('a');
+      $a.setAttribute('href', val);
+      $a.innerHTML = /.\/assets/g.test(val) ? val.substr(val.lastIndexOf('/') + 1) : val;
+
+      val = '"' + $a.outerHTML + '"';
+    }
+
+    $val = document.createElement('span');
+    $val.setAttribute('class', TYPES[type]);
+    $val.innerHTML = val;
+    body += $val.outerHTML;
+  }
+
+  return head + body + tail;
+}
+
+function prettyPrint (obj) {
+  var line = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+  return JSON.stringify(obj, null, 2).replace(/&/g, '&amp;').replace(/\\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(line, replacer);
+}
+
+window.onload = function () {
+  getJson('./assets/docs/kshvmdn.json', function(err, res) {
+    var $el, k;
+
+    if (err || !res) {
+      document.querySelector('.preload').style.display = 'block';
+      return;
+    }
+
+    for (k in res) {
+      $el = document.getElementById(k + '-json');
+      $el.innerHTML = prettyPrint(res[k]);
+    }
+
+    document.querySelector('.console').style.display = 'block';
+  });
+}
